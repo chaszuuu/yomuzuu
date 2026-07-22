5a2d5e2977dd9168ea4ada725912780549f8bb96
chaszuuu <bornbeast03@gmail.com>
2026-07-22 12:31:30 +0800

    fix: GitHub Actions CI workflow v3

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? [['html'], ['github']] : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: 'http://localhost:5173',
@@ -34,6 +34,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    
     /*
     {
       name: 'firefox',
@@ -44,7 +45,8 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
+    */
+   
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
